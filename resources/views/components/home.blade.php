018c5b18b4040d940b309766a844d59670275e65
Diego Ramos Gomes <diegoramosgomes5@gmail.com>
2020-06-18 18:40:37 -0300

    feat - adicionado home

---
+++ resources/views/components/home.blade.php
@@ -0,0 +1,32 @@
+<div class="d-flex justify-content-center align-items-center vh-100 bg-dark">
+    <div>
+        <img alt="Imagem de perfil" class="rounded-circle mx-auto d-block w-25"
+             src="{{ asset('storage/profile_photo.svg') }}">
+        <div class="text-center text-white my-3">
+            <h1>Diego Ramos Gomes</h1>
+            <p>Back-end / Mobile developer</p>
+        </div>
+        <ul class="list-inline text-center">
+            <li class="list-inline-item">
+                <a target="_blank" href="https://www.instagram.com/dihrgomes/" class="text-white">
+                    <i class="fa fa-instagram"></i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a target="_blank" href="https://github.com/DiegoRamosGomes/" class="text-white">
+                    <i class="fa fa-github"></i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a target="_blank" href="https://api.whatsapp.com/send?phone=5548991417009" class="text-white">
+                    <i class="fa fa-whatsapp"></i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a target="_blank" href="mailto:diegoramosgomes5@gmail.com" class="text-white">
+                    <i class="fa fa-envelope"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
